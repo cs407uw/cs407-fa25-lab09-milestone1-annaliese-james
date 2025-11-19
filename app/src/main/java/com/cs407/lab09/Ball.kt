24550325e34179a774113ee64fba14fef9c712cc
jamesgeorgelos <jgeorgelos@wisc.edu>
2025-11-18 19:06:21 -0600

    Milestone 1

--- app/src/main/java/com/cs407/lab09/Ball.kt
+++ app/src/main/java/com/cs407/lab09/Ball.kt
@@ -23,7 +23,7 @@ class Ball(
     private var isFirstUpdate = true
 
     init {
-        // TODO: Call reset()
+        reset()
     }
 
     /**
@@ -38,6 +38,32 @@ class Ball(
             return
         }
 
+        // Previous acceleration (a0)
+        val a0x = accX
+        val a0y = accY
+
+        // New acceleration (a1)
+        val a1x = xAcc
+        val a1y = yAcc
+
+        // X axis
+        val dx = velocityX * dT +
+                (1f / 6f) * dT * dT * (3f * a0x + a1x)
+        val newVx = velocityX + 0.5f * (a0x + a1x) * dT
+
+        posX += dx
+        velocityX = newVx
+
+        // Y axis
+        val dy = velocityY * dT + (1f / 6f) * dT * dT * (3f * a0y + a1y)
+        val newVy = velocityY + 0.5f * (a0y + a1y) * dT
+
+        posY += dy
+        velocityY = newVy
+
+        // Store latest acceleration for next step
+        accX = xAcc
+        accY = yAcc
     }
 
     /**
@@ -46,8 +72,33 @@ class Ball(
      * boundary should be set to 0.
      */
     fun checkBoundaries() {
-        // TODO: implement the checkBoundaries function
-        // (Check all 4 walls: left, right, top, bottom)
+        // Left wall
+        if (posX < 0f) {
+            posX = 0f
+            velocityX = 0f
+            accX = 0f
+        }
+
+        // Right wall
+        if (posX + ballSize > backgroundWidth) {
+            posX = backgroundWidth - ballSize
+            velocityX = 0f
+            accX = 0f
+        }
+
+        // Top wall
+        if (posY < 0f) {
+            posY = 0f
+            velocityY = 0f
+            accY = 0f
+        }
+
+        // Bottom wall
+        if (posY + ballSize > backgroundHeight) {
+            posY = backgroundHeight - ballSize
+            velocityY = 0f
+            accY = 0f
+        }
     }
 
     /**
@@ -55,7 +106,16 @@ class Ball(
      * velocity and acceleration.
      */
     fun reset() {
-        // TODO: implement the reset function
-        // (Reset posX, posY, velocityX, velocityY, accX, accY, isFirstUpdate)
+        // Center of the field (posX/posY are top-left of the ball)
+        posX = (backgroundWidth - ballSize) / 2f
+        posY = (backgroundHeight - ballSize) / 2f
+
+        velocityX = 0f
+        velocityY = 0f
+        accX = 0f
+        accY = 0f
+
+        // Next sensor update will re-initialize acceleration
+        isFirstUpdate = true
     }
 }
